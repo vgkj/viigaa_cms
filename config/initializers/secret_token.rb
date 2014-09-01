62cf393b25104112ad9336e512658894e986cf4d
liangchiwen <liangchiwen@viigaa.com>
2014-09-01 10:19:12 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ViigaaCms::Application.config.secret_key_base = '7169ecdf2f12dc9d600fad1c83d8ce4e3a7e3ea56751eb1244b4c43f2bfdfa84830c8298b1ed7f1bee4e586e1fb99a31670833d1fe9b64327798154ec942e1b7'
